c9a8081e6bcbf21e8bda5c7d0c30c9e27226ca22
zhang-xiao-miao <1286874200@qq.com>
2018-09-09 00:28:11 -0400

    placeholder demo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Placeholder from './common/components/Placehoder/index';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Placeholder></Placeholder>
+      </div>
+    );
+  }
+}
+
+export default App
