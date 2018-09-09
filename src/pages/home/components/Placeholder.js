6a2b643763322c7e622a5cab13b53d0859e73714
zhang-xiao-miao <1286874200@qq.com>
2018-09-09 17:38:02 -0400

    placeholder for loading topics

---
+++ src/pages/home/components/Placeholder.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Loading from '../../../common/components/Loading'
+
+import './Placeholder.css'
+
+class Placehoder extends React.PureComponent {
+  render() {
+    return (
+      <div className="Placeholder bottom-bordered">
+        <div className="ph-item">
+          <div className="ph-row flex-row-between">
+            <div className="ph-col-2 blue"></div>
+            <div className="ph-col-2"></div>
+          </div>
+        </div>
+        <div className="ph-item">
+          <div className="ph-row">
+            <div className="ph-col-12 big"></div>
+          </div>
+        </div>
+        <div className="ph-item">
+          <div className="ph-row flex-row-between">
+            <div className="ph-col-2"></div>
+            <div className="ph-col-2"></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default class TopicPlaceholder extends React.Component {
+  render () {
+   return (
+    <div className="TopicPlaceholder">
+      <Loading></Loading>
+      <div className="placeholders">
+        <Placehoder></Placehoder>
+        <Placehoder></Placehoder>
+        <Placehoder></Placehoder>
+        <Placehoder></Placehoder>
+        <Placehoder></Placehoder>
+        <Placehoder></Placehoder>
+        <Placehoder></Placehoder>
+        <Placehoder></Placehoder>
+      </div>
+    </div>
+   )
+  }
+}
+
