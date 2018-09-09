1736c12d9d253537e6e2cb7075d4961331f4bf9d
zhang-xiao-miao <1286874200@qq.com>
2018-09-09 17:32:43 -0400

    add constants

---
+++ src/constants/post-types.js
@@ -0,0 +1,29 @@
+const POST_TYPES = [{
+  value: 'all',
+  label: '全部'
+}, {
+  value: 'good',
+  label: '精华'
+}, {
+  value: 'share',
+  label: '分享'
+}, {
+  value: 'ask',
+  label: '问答'
+}, {
+  value: 'job',
+  label: '招聘'
+}, {
+  value: 'dev',
+  label: '测试'
+}]
+
+export default POST_TYPES
+
+export const Tab_Types_Map = {
+  'good': '精华',
+  'share': '分享',
+  'ask': '问答',
+  'job': '招聘',
+  'dev': '测试'
+}
